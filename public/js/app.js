8fe589f5d1d246d3f7e851ce41dd1f51ede75014
Sam Rosenbluth <samuel.rosenbluth@gmail.com>
2017-07-26 23:53:46 -0500

    made the ccorrect components stateful. Still need to make the forms stateful.

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,31 @@
 class TimersDashboard extends React.Component{
+  state = {
+    timers: [
+      {
+        title: 'Practice squat',
+        project: 'Gym Chores',
+        id: uuid.v4(),
+        elapsed: 5456099,
+        runningSince: Date.now(),
+      },
+      {
+        title: 'Bake squash',
+        project: 'Gym Chores',
+        id: uuid.v4(),
+        elapsed: 1273998,
+        runningSince: null,
+      },
+    ],
+  };
+
+
   render(){
     return(
       <div className='ui three column centered grid'>
         <div className='column'>
-          <EditableTimerList/>
-          <ToggleableTimerForm
-            isOpen={true}
-          />
+          <EditableTimerList
+          timers={this.state.timers}/>
+          <ToggleableTimerForm />
       </div>
     </div>
     )
@@ -15,32 +34,34 @@ class TimersDashboard extends React.Component{
 
 class EditableTimerList extends React.Component{
   render(){
+    const timers = this.props.timers.map((timer) => (
+      <EditableTimer
+        key={timer.id}
+        id={timer.id}
+        title={timer.title}
+        project={timer.project}
+        elapsed={timer.eplased}
+        runningSince={timer.runningSince}
+      />
+    ));
     return(
       <div id='timers'>
-        <EditableTimer
-          title='Learn React'
-          project='World Domination'
-          elapsed='8986300'
-          runningSince={null}
-          editFormOpen={false}
-        />
-        <EditableTimer
-          title="Learn extreme ironing"
-          project="World Domination"
-          elapsed='3890985'
-          runningSince={null}
-          editFormOpen={true}
-        />
+        {timers}
       </div>
-    );
+      );
   }
 }
 
 class EditableTimer extends React.Component {
+  state = {
+    editFormOpen: false,
+  };
+
   render() {
     if(this.props.editFormOpen){
       return (
         <TimerForm
+          id={this.props.id}
           title={this.props.title}
           project={this.props.project}
         />
@@ -48,6 +69,7 @@ class EditableTimer extends React.Component {
     } else {
       return (
         <Timer
+          id={this.props.id}
           title={this.props.title}
           project={this.props.project}
           elapsed={this.props.elapsed}
@@ -89,15 +111,25 @@ const submitText = this.props.title ? 'Update' : 'Create';
 }
 
 class ToggleableTimerForm extends React.Component {
+  state = {
+    isOpen: false,
+  };
+  handleFormOpen = () => {
+    this.setState({ isOpen: true });
+  };
+
   render(){
-    if (this.props.isOpen) {
+    if (this.state.isOpen) {
       return (
         <TimerForm />
       );
     } else {
       return (
         <div className='ui basic content center aligned segment'>
-          <button className='ui basic button icon'>
+          <button
+            className='ui basic button icon'
+            onClick={this.handleFormOpen}
+            >
             <i className='plus icon'/>
           </button>
         </div>
