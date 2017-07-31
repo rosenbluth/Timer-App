c333b3bec84caacecb729d5477cfe4c662df62d6
Sam Rosenbluth <samuel.rosenbluth@gmail.com>
2017-07-31 14:46:56 -0500

    the CRUD for the timers is built need to make the timers functional.

--- public/js/app-5.js
+++ public/js/app-5.js
@@ -202,49 +202,43 @@ class EditableTimer extends React.Component {
 }
 
 class Timer extends React.Component {
-  handleTrashClick = () => {
-    this.props.onTrashClick(this.props.id);
-  };
+    handleTrashClick = () => {
+        this.props.onTrashClick(this.props.id);
+    };
 
-  render() {
-    // ...
-    const elapsedString = helpers.renderElapsedString(this.props.elapsed);
-    return (
-      <div className='ui centered card'>
-        <div className='content'>
-          <div className='header'>
-            {this.props.title}
-          </div>
-          <div className='meta'>
-            {this.props.project}
-          </div>
-          <div className='center aligned description'>
-            <h2>
-              {elapsedString}
-            </h2>
-          </div>
-          {/* Inside Timer.render() */}
-          <div className='extra content'>
-            <span
-              className='right floated edit icon'
-              onClick={this.props.onEditClick}
-            >
-              <i className='edit icon' />
-            </span>
-            <span
-              className='right floated trash icon'
-              onClick={this.handleTrashClick}
-            >
-              <i className='trash icon' />
-            </span>
-          </div>
-        </div>
-        <div className='ui bottom attached blue basic button'>
-          Start
-        </div>
-      </div>
-    );
-  }
+    render() {
+        // ...
+        const elapsedString = helpers.renderElapsedString(this.props.elapsed);
+        return (
+            <div className='ui centered card'>
+                <div className='content'>
+                    <div className='header'>
+                        {this.props.title}
+                    </div>
+                    <div className='meta'>
+                        {this.props.project}
+                    </div>
+                    <div className='center aligned description'>
+                        <h2>
+                            {elapsedString}
+                        </h2>
+                    </div>
+                    {/* Inside Timer.render() */}
+                    <div className='extra content'>
+                        <span className='right floated edit icon' onClick={this.props.onEditClick}>
+                            <i className='edit icon'/>
+                        </span>
+                        <span className='right floated trash icon' onClick={this.handleTrashClick}>
+                            <i className='trash icon'/>
+                        </span>
+                    </div>
+                </div>
+                <div className='ui bottom attached blue basic button'>
+                    Start
+                </div>
+            </div>
+        );
+    }
 }
 
 class TimerForm extends React.Component {
--- public/js/app.js
+++ public/js/app.js
@@ -16,10 +16,20 @@ class TimersDashboard extends React.Component {
             },
         ],
       };
+
       handleEditFormSubmit = (attrs) => {
       this.updateTimer(attrs);
     };
 
+    handleTrashClick = (timerId) => {
+      this.deleteTimer(timerId);
+    }
+
+    deleteTimer = (timerId) => {
+      this.setState({
+        timers: this.state.timers.filter(t => t.id !== timerId),
+      });
+    };
         handleCreateFormSubmit = (timer) => {
           this.createTimer(timer);
         }
@@ -54,6 +64,7 @@ class TimersDashboard extends React.Component {
                     <EditableTimerList
                       timers={this.state.timers}
                       onFormSubmit={this.handleEditFormSubmit}
+                      onTrashClick={this.handleTrashClick}
                     />
                 <ToggleableTimerForm
                 onFormSubmit={this.handleCreateFormSubmit}
@@ -73,6 +84,7 @@ class EditableTimerList extends React.Component {
           project={timer.project}
           elapsed={timer.elapsed} runningSince={timer.runningSince}
           onFormSubmit={this.props.onFormSubmit}
+          onTrashClick={this.props.onTrashClick}
         />
       ));
         return (
@@ -127,6 +139,7 @@ class EditableTimer extends React.Component {
           elapsed={this.props.elapsed}
           runningSince={this.props.runningSince}
           onEditClick={this.handleEditClick}
+          onTrashClick={this.props.onTrashClick}
         />
       );
     }
@@ -139,12 +152,13 @@ class TimerForm extends React.Component {
         project: this.props.project || ''
     };
     handleTitleChange = (e) => {
-        this.setState({title: e.target.value});
+        this.setState({ title: e.target.value });
     };
 
     handleProjectChange = (e) => {
-        this.setState({project: e.target.value});
+        this.setState({ project: e.target.value });
     };
+
     handleSubmit = () => {
       this.props.onFormSubmit({
         id: this.state.id,
@@ -228,6 +242,10 @@ class ToggleableTimerForm extends React.Component {
 }
 
 class Timer extends React.Component {
+    handleTrashClick = () => {
+        this.props.onTrashClick(this.props.id);
+    };
+
     render() {
         const elapsedString = helpers.renderElapsedString(this.props.elapsed);
         return (
@@ -245,12 +263,10 @@ class Timer extends React.Component {
                         </h2>
                     </div>
                     <div className="extra content">
-                        <span className="right floated edit icon"
-                          onClick={this.props.onEditClick}
-                          >
+                        <span className="right floated edit icon" onClick={this.props.onEditClick}>
                             <i className="edit icon"/>
                         </span>
-                        <span className='right floated trash icon'>
+                        <span className='right floated trash icon' onClick={this.handleTrashClick}>
                             <i className='trash icon'/>
                         </span>
                     </div>
